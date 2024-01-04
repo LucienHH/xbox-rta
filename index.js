1e559c3d0d50c1dd999690791f32e6426b289ff9
unknown <lucienharryholloway@hotmail.co.uk>
2024-01-04 00:55:37 +0000

    Rewrite to TS

--- index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	XboxRTA: require('./src/index'),
-};
---
+++ src/common/constants.ts
@@ -0,0 +1,34 @@
+export enum MessageType {
+	Subscribe = 1,
+	Unsubscribe,
+	Event,
+	Resync,
+}
+
+export enum StatusCode {
+	Success = 0,
+	UnknownResource = 1,
+	SubscriptionLimitReached = 2,
+	NoResourceData = 3,
+	Throttled = 1001,
+	ServiceUnavailable = 1002
+}
+
+export const convertRTAStatus = (status: number) => {
+  switch (status) {
+    case StatusCode.Success:
+      return 'Success'
+    case StatusCode.UnknownResource:
+      return 'UnknownResource'
+    case StatusCode.SubscriptionLimitReached:
+      return 'SubscriptionLimitReached'
+    case StatusCode.NoResourceData:
+      return 'NoResourceData'
+    case StatusCode.Throttled:
+      return 'Throttled'
+    case StatusCode.ServiceUnavailable:
+      return 'ServiceUnavailable'
+    default:
+      return 'Unknown'
+  }
+}
